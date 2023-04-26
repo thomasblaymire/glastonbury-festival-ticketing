dd1ba1adf53fad3753b59b819931c00c32ef485e
Tom Blaymire <thomasblaymire@gmail.com>
2023-04-26 12:05:43 +0100

    feat(app): implement base serverless setup and add additinoal dependencies

---
+++ webpack.config.ts
@@ -0,0 +1,32 @@
+import path from 'path'
+import nodeExternals from 'webpack-node-externals'
+import { Configuration } from 'webpack'
+
+const config: Configuration = {
+  entry: {
+    'release/index': './lambda/release/index.ts',
+    'queue/index': './lambda/queue/index.ts',
+  },
+  externals: [nodeExternals()],
+  target: 'node',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+}
+
+export default config
